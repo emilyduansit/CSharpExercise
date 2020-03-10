0eaaf63a25c8d186f10fe3b6fc926a87e2d78b90
emilyduansit <Emily.duan.co@gmail.com>
2020-03-11 10:28:02 +1300

    add class practice

---
+++ Class_Student+Teacher/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Person
+{
+    protected int age;
+    public void SetAge(int n)
+    {
+        age = n;
+        Console.WriteLine("My age is {0} years old",age);
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine("Hello Everyone");
+    }
+
+
+}
+class Student : Person
+{
+    public void GoToClass()
+    {
+        Console.WriteLine("I will go to class");
+    }
+
+    public void ShowAge()
+    {
+        Console.WriteLine("I am {0} years old",age);
+    }
+}
+class Teacher : Person
+{
+    private string subject;
+    public void Explain()
+    {
+        Console.WriteLine("Explanation begins");
+    }
+}
+
+namespace Class_Student_Teacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Person person = new Person();
+           Student student = new Student();
+           Teacher teacher = new Teacher();
+
+            person.Greet();
+            student.SetAge(21);
+            student.ShowAge();
+
+            teacher.SetAge(30);
+            teacher.Greet();
+            teacher.Explain();
+
+        }
+    }
+}
